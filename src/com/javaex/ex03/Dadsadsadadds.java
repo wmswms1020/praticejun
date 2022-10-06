f168fade0abbfe63dc5574c597cfccae8fa92473
wmswm <wmswm@172.30.1.42>
2022-10-06 17:13:21 +0900

    dddd

---
+++ src/com/javaex/ex03/Dadsadsadadds.java
@@ -0,0 +1,14 @@
+package com.javaex.ex03;
+
+public class Dadsadsadadds {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public 
+		a
+		
+		sdadadsd
+		
+	}dsds
+
+}
